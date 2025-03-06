702480f253c4ea053518a828dba03e412c9782f1
hoa.truong <hoa.truong@fssc.com.vn>
2025-03-07 01:29:03 +0700

    update(section 3): add polling http for catch and retry request

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -14,13 +17,25 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
-{
-    await DbInitializer.InitializeAsync(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+/* Make process initailize db fucntion async , If it fails system will run next process and retry later */
+app.Lifetime.ApplicationStarted.Register(
+    async () =>
+    {
+        try
+        {
+            await DbInitializer.InitializeAsync(app);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    });
 
 app.Run();
+
+/* Add policy for catch exception and retry http request */
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
